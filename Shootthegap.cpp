7b696537b01f18e98c77b35056a30b9cd4918932
Luka Bitsadze <bitsadzeluka@gmail.com>
2023-07-05 23:52:26 +0400

    Update Shootthegap.cpp

--- Shootthegap.cpp
+++ Shootthegap.cpp
@@ -2,42 +2,30 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <exception>
 
 #define SCREENX 800
 #define SCREENY 600
 
-class GameObject {
+
+class Transformable {
+protected:
+    sf::RectangleShape shape;
 public:
     virtual void move() {};
     virtual void draw() {};
     virtual void update() {};
+    void setPosition( sf::Vector2f& position) {shape.setPosition(position);}
+
+    const sf::Vector2f& getPosition()  {return shape.getPosition();}
+
+    sf::RectangleShape& getShape() {return shape;}
+
+    const sf::RectangleShape& getShape() const {return shape;}
+
 
 };
 
-class Transformable {
-public:
-    void setPosition(const sf::Vector2f& position) {
-        shape.setPosition(position);
-    }
-
-    const sf::Vector2f& getPosition()  {
-        return shape.getPosition();
-    }
-
-    sf::RectangleShape& getShape() {
-        return shape;
-    }
-
-    const sf::RectangleShape& getShape() const {
-        return shape;
-    }
-
-protected:
-    sf::RectangleShape shape;
-};
-
-class Figure : public Transformable, public GameObject {
+class Figure : public Transformable {
 public:
     Figure(float sizeX, float sizeY, sf::Color color) {
         shape.setSize(sf::Vector2f(sizeX, sizeY));
@@ -46,34 +34,27 @@ public:
 };
 
 class Bullet : public Figure {
+private:
+    sf::Vector2f velocity;
 public:
     Bullet(float sizeX, float sizeY, sf::Color color, float speed) : Figure(sizeX, sizeY, color), velocity(speed, 0.0f) {}
 
-    void move() {
-        shape.move(velocity);
-    }
+    void move() {shape.move(velocity);}
 
-    bool operator==(const Bullet& other) const {
-        return shape.getPosition() == other.shape.getPosition() &&
-            velocity == other.velocity;
-    }
+    bool operator==(const Bullet& other) const {return shape.getPosition() == other.shape.getPosition() && velocity == other.velocity;}
 
-private:
-    sf::Vector2f velocity;
+
 };
 
-class Player : public Figure, public GameObject {
+class Player : public Figure{
 public:
-    Player() : Figure(0, 0, sf::Color::Transparent) {}
-
-    Player(float sizeX, float sizeY, sf::Color color, const sf::Vector2f& position) : Figure(sizeX, sizeY, color) {
-        shape.setPosition(position);
-    }
+    Player() : Figure(0, 0, sf::Color::Transparent) {};
+    Player(float sizeX, float sizeY, sf::Color color, const sf::Vector2f& position) : Figure(sizeX, sizeY, color) {shape.setPosition(position);}
 };
 
-class Target : public Figure, public GameObject {
+class Target : public Figure {
 public:
-    Target() : Figure(0, 0, sf::Color::Transparent) {}
+    Target() : Figure(0, 0, sf::Color::Transparent) {};
 
     Target(float sizeX, float sizeY, sf::Color color, const sf::Vector2f& position) : Figure(sizeX, sizeY, color) {
         shape.setPosition(position);
@@ -82,45 +63,6 @@ public:
 
 
 class Game {
-public:
-    Game() : window(sf::VideoMode(SCREENX, SCREENY), "Go lets play") {
-        window.setFramerateLimit(60);
-
-        player = Player(50, 50, sf::Color::Blue, sf::Vector2f(100, 0));
-
-        target = Target(30, 70, sf::Color::Red, sf::Vector2f(600, 0));
-        
-        for (float y = 0; y <= SCREENY; y += 100) {
-            targets.push_back(Target(30, 70, sf::Color::Red, sf::Vector2f(600, y)));
-        }
-
-        const float bulletSpeed = 5.0f;
-        this->bulletSpeed = bulletSpeed;
-
-        direction = 1; 
-
-        gameOver = false;
-        score = 0;
-
-        scoreLine.setSize(sf::Vector2f(2, SCREENY));
-        scoreLine.setFillColor(sf::Color::Green);
-        scoreLine.setPosition(630, 0);
-    }
-
-    void run() {
-        try {
-            while (window.isOpen()) {
-                processEvents();
-                update();
-                render();
-            }
-            std::cout << score << std::endl;
-        }
-        catch (const std::exception& e) {
-            std::cerr << "Exception occurred: " << e.what() << std::endl;
-        }
-    }
-
 private:
     void processEvents() {
         sf::Event event;
@@ -130,20 +72,10 @@ private:
             }
             else if (event.type == sf::Event::KeyPressed) {
                 if (event.key.code == sf::Keyboard::Space) {
-                    try {
                         shootBullet();
-                    }
-                    catch (const std::exception& e) {
-                        std::cerr << "Exception occurred while shooting bullet: " << e.what() << std::endl;
-                    }
                 }
                 else if (event.key.code == sf::Keyboard::Q) {
-                    try {
                         quitGame();
-                    }
-                    catch (const std::exception& e) {
-                        std::cerr << "Exception occurred while quitting the game: " << e.what() << std::endl;
-                    }
                 }
             }
         }
@@ -181,7 +113,7 @@ private:
             if (bullet.getPosition().x > window.getSize().x)
                 collidedBullets.push_back(bullet);
 
-            
+
             for (auto& target : targets) {
                 sf::FloatRect bulletBounds = bullet.getShape().getGlobalBounds();
                 sf::FloatRect targetBounds = target.getShape().getGlobalBounds();
@@ -195,7 +127,7 @@ private:
 
                 }
             }
-            
+
 
             sf::FloatRect bulletBounds = bullet.getShape().getGlobalBounds();
             sf::FloatRect scoreLineBounds = scoreLine.getGlobalBounds();
@@ -206,11 +138,13 @@ private:
             }
         }
 
+        
         for (const auto& collidedBullet : collidedBullets) {
             auto it = std::find(bullets.begin(), bullets.end(), collidedBullet);
             if (it != bullets.end())
                 bullets.erase(it);
         }
+        
     }
 
     void render() {
@@ -242,11 +176,50 @@ private:
     std::vector<Target> targets;
     std::vector<Bullet> bullets;
     sf::RectangleShape scoreLine;
-
     int direction;
     bool gameOver;
     int score;
     float bulletSpeed;
+public:
+    Game() : window(sf::VideoMode(SCREENX, SCREENY), "Go lets play") {
+        window.setFramerateLimit(60);
+
+        player = Player(50, 50, sf::Color::Blue, sf::Vector2f(100, 0));
+
+        target = Target(30, 70, sf::Color::Red, sf::Vector2f(600, 0));
+        
+        for (float y = 0; y <= SCREENY; y += 100) {
+            targets.push_back(Target(30, 70, sf::Color::Red, sf::Vector2f(600, y)));
+        }
+
+        const float bulletSpeed = 5.0f;
+        this->bulletSpeed = bulletSpeed;
+
+        direction = 1; 
+
+        gameOver = false;
+        score = 0;
+
+        scoreLine.setSize(sf::Vector2f(2, SCREENY));
+        scoreLine.setFillColor(sf::Color::Green);
+        scoreLine.setPosition(630, 0);
+    }
+
+    void run() {
+        try {
+            while (window.isOpen()) {
+                processEvents();
+                update();
+                render();
+            }
+            std::cout << score << std::endl;
+        }
+        catch (const std::exception& e) {
+            std::cerr << "Exception occurred: " << std::endl;
+        }
+    }
+
+
 };
 
 int main() {
@@ -254,4 +227,3 @@ int main() {
     game.run();
     return 0;
 }
-
